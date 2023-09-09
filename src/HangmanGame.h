05c4b243d330ea0597ada02c24eed294a5800d9b
Rustam Ibragimov <ibragimovich.rustamovichov27963@mail.ru>
2023-09-09 17:57:36 +0700

    Wrote MakeFile and set the project structure

---
+++ src/HangmanGame.h
@@ -0,0 +1,64 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+class HangmanGame {
+public:
+    HangmanGame();
+    void Init();
+    void ProcessInput(char guess);
+    void Draw(sf::RenderWindow& window);
+    void Reset();
+    bool IsGameWon() const {
+        return guessedWord == secretWord;
+    }
+    bool IsGameOver() const {
+        return attemptsLeft == 0 || IsGameWon();
+    }
+    std::string GetSecretWord() const {
+        return secretWord;
+    }
+    enum class GameState {
+        Playing,
+        Win,
+        Lose
+    };
+    std::string GetGuessedWord() const {
+        return guessedWord;
+    }
+
+    int GetAttemptsLeft() const {
+        return attemptsLeft;
+    }
+    GameState GetGameState() const {
+        return gameState;
+    }
+    
+private:
+    std::string targetWord;
+    std::string secretWord;
+    std::string guessedWord;
+    int attemptsLeft;
+    bool isGameOver;
+    bool isGameWon;
+    GameState gameState = GameState::Playing;
+
+    sf::Font font;
+    sf::Text wordText;
+    sf::Text attemptsText;
+    sf::Text resultText;
+
+    sf::SoundBuffer correctGuessSoundBuffer;
+    sf::SoundBuffer wrongGuessSoundBuffer;
+    sf::SoundBuffer winSoundBuffer;
+    sf::SoundBuffer loseSoundBuffer;
+
+    sf::Sound correctGuessSound;
+    sf::Sound wrongGuessSound;
+    sf::Sound winSound;
+    sf::Sound loseSound;
+    sf::Texture backgroundTexture;
+    sf::Sprite backgroundSprite;
+    void CheckForWin();
+    void UpdateGuessedWord(std::size_t index, char guess);
+    void LoadSounds();
+};
